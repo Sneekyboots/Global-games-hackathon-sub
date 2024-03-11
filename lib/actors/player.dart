4fdabe64d7fcbbe4144c84bd647b7d0e1b142c05
SneekyBoots <sneekyboots122@gmail.com>
2024-03-11 20:39:15 +0530

    code made efficient and added all the charecters added postion spawns

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,26 +1,41 @@
 import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_games/pixel_adventure.dart';
+import 'package:flutter/cupertino.dart';
 
-enum PlayerState {idle, running }
+enum PlayerState { idle, running }
+
 //used group cause we have lots of animations
-class Player extends SpriteAnimationGroupComponent with HasGameRef<PixelAdventure>{
+class Player extends SpriteAnimationGroupComponent
+    with HasGameRef<PixelAdventure> {
+  String character;
+  Player({ position,  required this.character}) : super(position: position);
+
+
   late final SpriteAnimation idleAnimation;
-  final double stepTime= 0.05;
+  late final SpriteAnimation runningani;
+  final double stepTime = 0.05;
+
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
   }
-void _loadAllAnimations(){
-  idleAnimation =SpriteAnimation.fromFrameData(game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
-      SpriteAnimationData.sequenced(amount: 11,
-          stepTime: stepTime, textureSize: Vector2.all(32)));
-  //list of all the animations
-  animations = {
-    PlayerState.idle :idleAnimation
-  };
-  //set  current animation
-  current =PlayerState.idle;
-}}
+  void _loadAllAnimations() {
+    idleAnimation = _spriteAnimation('Idle',11);
+    runningani = _spriteAnimation('Run',12);
+    //list of all the animations
+    animations = {PlayerState.idle: idleAnimation,
+                  PlayerState.running:runningani,
+
+    };
+    current = PlayerState.running;
+  }
+  SpriteAnimation _spriteAnimation(String state ,int amount){
+    return SpriteAnimation.fromFrameData(game.images.fromCache('Main Characters/$character/$state (32x32).png'),
+        SpriteAnimationData.sequenced(amount: amount,
+            stepTime: stepTime, textureSize: Vector2.all(32)));
+
+  }
+}
