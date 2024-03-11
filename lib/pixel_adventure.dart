43b9c6fd3a3332c838b2a26e60e1288668fa702e
SneekyBoots <sneekyboots122@gmail.com>
2024-03-11 19:37:41 +0530

    added the player the ninja fron

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,17 +1,29 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_games/levels/level.dart';
 
 class PixelAdventure extends FlameGame{
-  //issues
+  @override
+  Color backgroundColor()=> const Color(0xFF211F30);
   late final CameraComponent cam;
+
   @override
   final world=Level();
+
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    //we will be loading all the images of the character for now in cache
+
+    await images.loadAllImages();
+
+
     cam= CameraComponent.withFixedResolution(world:world, width: 640, height:360 );
+    cam.viewfinder.anchor=Anchor.topLeft;
     addAll([cam,world]);
+
+
     return super.onLoad();
   }
 }
